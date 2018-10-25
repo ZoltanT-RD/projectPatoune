c0ccd9e3a67e12b484b331a9d24c6737898a0660
ZoltanTompa-ResDiary <zoltan@resdiary.com>
2018-10-25 11:00:45 +0100

    playing with opening/closing book-details view (in progress)

--- js/dev.js
+++ js/dev.js
@@ -1,1 +1,16 @@
 var isDebugEnabled = true;
+
+
+function openBookDetails(caller){
+    console.warn("you clicked me!");
+    console.warn(caller);
+
+    if ($(caller).hasClass("book-details-container")) {
+        $(caller).addClass("col s12 m6 l4");
+        $(caller).removeClass("book-details-container");
+    }
+    else{
+        $(caller).removeClass("col s12 m6 l4");
+        $(caller).addClass("book-details-container");
+    }
+}
