609aee8280d19549d99f3744f9747119c8eabc1e
ZoltanTompa-ResDiary <zoltan@resdiary.com>
2018-11-09 13:51:47 +0000

    now can search for author and/or book title, returning first 10

--- js/bookFetcher.js
+++ js/bookFetcher.js
@@ -26,7 +26,7 @@ maxResults - The maximum number of results to return. The default is 10, and the
 
 
 let bookFetcher = {
-    GoogleBooksAPIkey: 'AIzaSyCSnMvBVmL7KFOeEqtDWeQsiQuN8SCCxPo',
+    rawOutput: '',
 
     sayHello: function () {
         console.warn("Hello =)");
@@ -34,19 +34,99 @@ let bookFetcher = {
     jqTester: function(){
         $("#main p").css("color","orange");
     },
-    fetchTestBook: function(){
+    searchBook: function(urlParams,callback){
         $.ajax({
-            url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:the+power+of+a+positive+no',
+            url: 'https://www.googleapis.com/books/v1/'+urlParams,
             type: 'GET',
             dataType: 'json',
-            success: function(json) { console.log(json) },
+            success: function(json) { callback(bookFetcher,json) },
             error: function(err) { console.error(err) }
         });
+    },
+
+    searchBookByTitleAndAuthor: function(author, title){
+        console.warn(bookFetcher.rawOutput = 'Searching for '+title+' by '+author);
+        bookFetcher.rawOutput = 'Searching for "'+title+'" by "'+author+'"';
+        let slug = 'volumes?q=';
+
+        if (author.length === 0 && title.length === 0) {
+            console.error("Both Title and Author can NOT be empty!");
+            bookFetcher.rawOutput = "Both Title and Author can NOT be empty!";
+            bookFetcher.renderOutput();
+            return;
+        }
+        if (author.length !=0 ) {
+            slug += 'inauthor:'+ author;
+        }
+        if (title.length != 0 ) {
+            slug += 'intitle:'+title;
+        }
+
+        bookFetcher.searchBook(slug,bookFetcher.buildResults);
+    },
+    searchTestBook: function(){
+        bookFetcher.searchBook('volumes?q=intitle:the+power+of+a+positive+no',function(tmp,json){console.log(json)});
+    },
+    buildResults: function(self,rawJson){
+        console.log("raw json pased in:");
+        console.log(rawJson);
+
+        if (rawJson.totalItems == 0) {
+            bookFetcher.rawOutput += "NO RESULTS FOUND...";
+        }
+        else{
+            for (let index = 0; index < rawJson.items.length; index++) {
+                const item = rawJson.items[index];
+                self.buildResult(item);
+            }
+        }
+        bookFetcher.renderOutput();
+    },
+    buildResult: function(item){
+        bookFetcher.rawOutput += '<div class="result">';
+        bookFetcher.rawOutput += '<img src="'+ bookFetcher.inserTextOrNA(item,["volumeInfo","imageLinks","thumbnail"],"https://via.placeholder.com/200x300") +'" />';
+        bookFetcher.rawOutput += '<p><span class="bookTitle">'+ item.volumeInfo.title +'</span>-';
+        bookFetcher.rawOutput += '<span class="bookSubtitle">'+ bookFetcher.inserTextOrNA(item,["volumeInfo","subtitle"],"") +'</span>(';
+        bookFetcher.rawOutput += '<span class="publishYear">'+ bookFetcher.inserTextOrNA(item,["volumeInfo","publishedDate"],"n/a") +'</span>)</p>';
+        bookFetcher.rawOutput += '<p class="listOfAuthors">';
+        for (let index = 0; index < item.volumeInfo.authors.length; index++) {
+            const element = item.volumeInfo.authors[index];
+            bookFetcher.rawOutput += '<span class="bookAuthor">'+ element +'</span>';
+        }
+        bookFetcher.rawOutput += '</p>';
+        for (let index = 0; index < item.volumeInfo.industryIdentifiers.length; index++) {
+            const element = item.volumeInfo.industryIdentifiers[index];
+            bookFetcher.rawOutput += '<p>'+ element.type+': <span class="'+element.type+'">'+ element.identifier +'</span> |';
+        }
+        bookFetcher.rawOutput += '<p> GoogleID: <span class="googleID">'+ item.id +'</span></p>';
+        bookFetcher.rawOutput += '</div>';
+    },
+    inserTextOrNA: function(obj,propArray,NAtext){
+        if (typeof obj === undefined) {
+            return NAtext;
+        }
+        else{
+            let tmpObj = obj;
+            for (let index = 0; index < propArray.length; index++) {
+                const element = propArray[index];
+
+                if (typeof tmpObj[element] != "undefined") {
+                    tmpObj = tmpObj[element];
+                }
+                else{
+                    return NAtext;
+                }
+            }
+            return tmpObj;
+        }
+    },
+    renderOutput: function () {
+        $('#results').html("<p>"+bookFetcher.rawOutput+"</p>");
     }
 };
 
-$( document ).ready(function() {
+$(document).ready(function() {
     bookFetcher.sayHello();
     bookFetcher.jqTester();
-    bookFetcher.fetchTestBook();
+    bookFetcher.searchTestBook();
 });
