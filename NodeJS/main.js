2f20e6fe5c6c71c782f58f49f239895c425ff11d
ZoltanTompa-ResDiary <zoltan@resdiary.com>
2018-12-03 11:37:11 +0000

    adding http status codes

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -2,6 +2,13 @@ const http = require('http');
 const fs = require('fs');
 const request = require('request');
 
+/*
+https://adrianmejia.com/blog/2016/08/24/building-a-node-js-static-file-server-files-over-http-using-es6/
+*/
+
+
+
+
 //amazonAPI https://images-na.ssl-images-amazon.com/images/P/1720651353.jpg //isbn10
 //googleAPI https://www.googleapis.com/books/v1/volumes?q=isbn:1537732730 //this needs parsing json = items[0].imageLinks.thumbnail (none of the props are guaranteed)
 //openLib http://covers.openlibrary.org/b/isbn/9780451498298-L.jpg
@@ -10,24 +17,41 @@ const request = require('request');
 //also this works; https://images-na.ssl-images-amazon.com/images/P/1720651353.jpg
 //but both fail to find a lot...
 
+// maps file extention to MIME types
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml'
+//  '.pdf': 'application/pdf',
+//  '.doc': 'application/msword',
+//  '.eot': 'appliaction/vnd.ms-fontobject',
+//  '.ttf': 'aplication/font-sfnt'
+};
+
+const HTTPstatusCodes = {
+  ok: 200, //Standard response for successful HTTP requests
+  created: 201, //The request has been fulfilled, resulting in the creation of a new resource
+  accepted: 202, //The request has been accepted for processing, but the processing has not been completed.
+  noContent: 204, //The server successfully processed the request and is not returning any content
+  badRequest: 400, //The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing)
+  unauthorised: 401, //Similar to 403 Forbidden, but specifically for use when authentication is required and has failed or has not yet been provided. The response must include a WWW-Authenticate header field containing a challenge applicable to the requested resource.
+  forbidden: 403, //The request was valid, but the server is refusing action. The user might not have the necessary permissions for a resource, or may need an account of some sort.
+  notFound: 404, //The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible.
+  methodNotAllowed: 405, //A request method is not supported for the requested resource; for example, a GET request on a form that requires data to be presented via POST, or a PUT request on a read-only resource.
+  ImATeapot: 418, //This code was defined in 1998 as one of the traditional IETF April Fools' jokes,
+};
+
+//todo: implement teapot protocol/response! eg.; https://www.google.com/teapot
 
 var bookCoverIndex = [];
 
-
-//  listening http server
-
-/* simple version
-const server = http.createServer((req, resp) =>{
-    if (req.url === '/') {
-        resp.write('Hello there! =]');
-        resp.end();
-    }
-});
-*/
-
-
 const text = {
-  hello: 'Hello there! =]  Welcome to the cover-store API!',
+  hello: 'Hello there! =]  Welcome to the cover-store API! ≤',
   onlyGet: 'Only GET requests are allowed, sorry.... (not really ^^)',
   badRequest: 'not sure what you mean by this request...',
   me: 'I am the cover-store API!',
@@ -41,23 +65,27 @@ const server = http.createServer((req, resp) =>{
 
 function startServer() {
   server.listen(1966);
-  console.log("Listening on 1966");
+  console.log("Listening on port 1966");
 }
 
 
 function httpRouter(req,resp){
 
-  let reqObject = require('url').parse(req.url);
-
+  //only allow GET requests
   if (req.method !== 'GET') {
+    resp.statusCode = HTTPstatusCodes.methodNotAllowed;
     resp.write(text.hello);
     resp.write(text.onlyGet);
     resp.end();
   }
 
+  let reqObject = require('url').parse(req.url);
   let pathArray =  reqObject.pathname.split("/");
-  pathArray.shift(); //[0] is always empty, so remove that
 
+  pathArray.shift(); //[0] is always empty, so remove that
+  pathArray.forEach(function(element) { //convert strings to lowercase
+    element = element.toLowerCase;
+  });
 
 /*
   console.log(`the method of the request was: ${req.method}`);
@@ -68,34 +96,34 @@ function httpRouter(req,resp){
 
   switch (pathArray[0]) {
     case '':
+      resp.statusCode = HTTPstatusCodes.ok;
       resp.write(text.hello);
       break;
 
     case 'me':
+      resp.statusCode = HTTPstatusCodes.ok;
       resp.write(text.me);
       break;
 
     case 'bookcover':
       switch (pathArray[1]) {
         case 'isbn10':
-          resp.write(text.xNotImplemented('ISBN-10'));
           resp.write(`you were searching for: ISBN10 : *${reqObject.query}*`);
           console.log(`the query sting *${reqObject.query}* is *${isQueriedStringValid(reqObject.query,pathArray[1])}*`);
-
           break;
 
         case 'isbn13':
-          resp.write(text.xNotImplemented('ISBN-13'));
           resp.write(`you were searching for: ISBN13 : *${reqObject.query}*`);
           console.log(`the query sting *${reqObject.query}* is *${isQueriedStringValid(reqObject.query,pathArray[1])}*`);
-
           break;
 
         case 'asin':
+          resp.statusCode = HTTPstatusCodes.notFound;
           resp.write(text.xNotImplemented('ASIN'));
           break;
 
         case 'ean':
+          resp.statusCode = HTTPstatusCodes.notFound;
           resp.write(text.xNotImplemented('EAN'));
           break;
 
@@ -106,6 +134,7 @@ function httpRouter(req,resp){
       break;
 
     default:
+      resp.statusCode = HTTPstatusCodes.badRequest;
       resp.write(text.badRequest);
       resp.write(JSON.stringify(reqObject));
       break;
@@ -116,9 +145,9 @@ function httpRouter(req,resp){
 }
 
 function isQueriedStringValid(q,type){
-  let regNumbersOnly = /^\d+$/; //only numbers; //todo: this is still boroken, allows letters mixed in ...
+  let regNumbersOnly = "^[0-9]*$";
 
-console.log(`type of the Q: ${typeof parseInt(q)}`);
+  console.log(`type of the Q: ${typeof parseInt(q)}`);
 
   if (q && type && q !=='' && type !== '') {
     switch (type) {
