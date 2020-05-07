4d08363618590e762ae9dd6a0024bd9578112dca
Zoltan Tompa <zoltan@resdiary.com>
2020-05-07 15:38:18 +0100

    resetting the project + introducing new structure

--- old-stuff/NodeJS/coverDownloader.js
+++ old-stuff/NodeJS/coverDownloader.js
@@ -21,6 +21,8 @@ openLib http://covers.openlibrary.org/b/isbn/0385472579-L.jpg //isbn10 or 13
 
 */
 
+///fixme this module is a mess ... strip it down, and build it up from the ground; 1.download a file 2. try X times if fails 3. if that fails as well, pick the next source
+
 const fs = require('fs');
 const request = require('request');
 const https = require('https');
@@ -28,8 +30,6 @@ const https = require('https');
 const chatty = require('./chatty');
 const mimeTypes = require('./mimeTypes');
 const HTTPstatusCodes = require('./HTTPstatusCodes');
-
-
 
 const zout = {
     out: (msg) => { chatty.toConsole.out(msg) },
@@ -41,6 +41,10 @@ const zout = {
 
 //this has to be set for every module
 const moduleName = "coverDownloader";
+
+//helper functions
+Promise.wait = (time) => new Promise(resolve => setTimeout(resolve, time || 0));
+Promise.retry = (cont, fn, delay) => fn().catch(err => cont > 0 ? Promise.wait(delay).then(() => Promise.retry(cont - 1, fn, delay)) : Promise.reject(err));
 
 
 const externalApiUrls = {
@@ -74,75 +78,86 @@ function queryExternalApis(isbn10) {
 
     zout.info("querying external APIs");
 
-    let returnObj = {
-        isSuccessfull: null,
-        msg: null
-    };
-
     return new Promise((resolve, reject) => {
 
-        downloadAndSaveFile(externalApiUrls.amazon1(isbn10), `bookCovers/${isbn10}.jpg`)
-            .then((resp) => {
-                zout.info("download from Amazon1 was successfull!");
-                bookCoverIndex.push(isbn10);
-                returnObj.isSuccessfull = true;
-                resolve(returnObj);
+        tryAmazon(isbn10)
+        .then(resp=>{
+            resolve(resp);
+        })
+        .catch(err=>{
+            tryGoogle(isbn10)
+            .then(resp=>{
+                resolve(resp);
             })
-            .catch((resp) => {
-                zout.warn("download from Amazon1 FAILED");
-
-                ///todo since this is throwing 403s all the time, for no reason, try to add a 5x try with 2 sec wait, if all 5 fails, then move to google
-
-                zout.warn("tryin Google...");
-                getGoogleApiImageUrl(isbn10)
-                    .then((val) => {
-                        downloadAndSaveFile(val.url, `bookCovers/${isbn10}.jpg`)
-                            .then((val) => {
-                                zout.info("download from Google was successfull!");
-                                bookCoverIndex.push(isbn10);
-                                returnObj.isSuccessfull = true;
-                                resolve(returnObj);
-                            }).catch((err) => {
-                                zout.warn("download from Goolge FAILED");
-
-                                zout.warn("tryin openLib...");
-
-                                downloadAndSaveFile(externalApiUrls.openLib(isbn10), `bookCovers/${isbn10}.jpg`, ['fileSize']).then(
-                                    (val) => {
-                                        zout.info("download from openLib was successfull!");
-                                        bookCoverIndex.push(isbn10);
-                                        returnObj.isSuccessfull = true;
-                                        resolve(returnObj);
-                                    }).catch((err) => {
-                                        returnObj.msg = `none of the externail APIs found the cover ${isbn10}...` + chatty.emoticons["(-_-')"];
-                                        zout.error(returnObj.msg);
-                                        returnObj.isSuccessfull = false;
-                                        reject(returnObj);
-                                    });
-                            });
-
-                    })
-                    .catch((err) => {
-                        zout.warn("download from Goolge FAILED");
-
-                        zout.warn("tryin openLib...");
-
-                        downloadAndSaveFile(externalApiUrls.openLib(isbn10), `bookCovers/${isbn10}.jpg`, ['fileSize']).then(
-                            (val) => {
-                                zout.info("download from openLib was successfull!");
-                                bookCoverIndex.push(isbn10);
-                                returnObj.isSuccessfull = true;
-                                resolve(returnObj);
-                            }).catch((err) => {
-                                returnObj.msg = `none of the externail APIs found the cover ${isbn10}...` + chatty.emoticons["(-_-')"];
-                                zout.error(returnObj.msg);
-                                returnObj.isSuccessfull = false;
-                                reject(returnObj);
-                            });
-                    });
-            });
+            .catch(err=>{
+                tryOpenLib(isbn10)
+                .then(resp=>{
+                    resolve(resp);
+                })
+                .catch(err=>{
+                    zout.error(`none of the externail APIs found the cover ${isbn10}...` + chatty.emoticons["(-_-')"]);
+                    reject(err);
+                })
+            })
+        })
     });
 };
+
+
+function tryAmazon(isbn10){
+    zout.info("Trying Amazon");
+    return new Promise((resolve, reject) => {
+        downloadHandler(externalApiUrls.amazon1(isbn10), `bookCovers/${isbn10}.jpg`)
+        .then((resp) => {
+            zout.info("download from Amazon1 was successfull!");
+            bookCoverIndex.push(isbn10);
+            resolve(resp);
+        })
+        .catch((resp) => {
+            zout.warn("download from Amazon FAILED");
+            reject(resp);
+    });
+});
+}
+
+function tryGoogle(isbn10){
+    zout.warn("tryin Google...");
+    return new Promise((resolve, reject) => {
+    getGoogleApiImageUrl(isbn10)
+        .then((val) => {
+            downloadHandler(val.url, `bookCovers/${isbn10}.jpg`)
+                .then((val) => {
+                    zout.info("download from Google was successfull!");
+                    bookCoverIndex.push(isbn10);
+                    returnObj.isSuccessfull = true;
+                    resolve(returnObj);
+                }).catch((err) => {
+                    zout.warn("download from Goolge FAILED");
+                    reject(err);
+                });
+            })
+        .catch(err=>{
+            zout.warn("download from Goolge Image Link FAILED");
+            reject(err);
+        });
+    });
+}
+
+function tryOpenLib(isbn10){
+    zout.warn("tryin openLib...");
+
+    downloadHandler(externalApiUrls.openLib(isbn10), `bookCovers/${isbn10}.jpg`, ['fileSize']).then(
+        (val) => {
+            zout.info("download from openLib was successfull!");
+            bookCoverIndex.push(isbn10);
+            resolve(val);
+        }).catch((err) => {
+            returnObj.msg = `none of the externail APIs found the cover ${isbn10}...` + chatty.emoticons["(-_-')"];
+            reject(err);
+        });
+}
+
+
 
 function getGoogleApiImageUrl(isbn) {
 
@@ -237,24 +252,25 @@ function getGoogleApiImageUrl(isbn) {
     });
 };
 
+
 function downloadHandler(uri, filename, ignore = []) {
 
-    ///todo maybe try this? https://stackoverflow.com/questions/42429590/retry-on-javascript-promise-reject-a-limited-number-of-times-or-until-success
+    //using this https://stackoverflow.com/questions/42429590/retry-on-javascript-promise-reject-a-limited-number-of-times-or-until-success
 
-    let failCounter = 0;
+    var delay = 1000;
+    var tries = 5;
 
-    downloadAndSaveFile(uri, filename, ignore = [])
-        .then((resp) => { })
-        .catch((resp) => { });
+    return new Promise((resolve, reject) => {
 
+        Promise.retry(tries, downloadAndSaveFile(uri, filename, ignore), delay)
+            .then((res)=>{
+                resolve(res);
+            })
+            .catch((res)=>{
+                reject(res);
+            });
+        });
 
-    if (failCounter < 5) {
-        setTimeout(function () {
-            zout.warn("trying again. with failcounter:" + failCounter++);
-            downloadAndSaveFile(uri, filename, ignore, (failCounter++));
-        }, 1500) //try again after 1.5 seconds
-
-    }
 }
 
 function downloadAndSaveFile(uri, filename, ignore = []) {
@@ -284,7 +300,7 @@ function downloadAndSaveFile(uri, filename, ignore = []) {
         }
 
         request.head(options, (err, res, body) => {
-            zout.info(`download request for ${filename} from ${uri} :`);
+            zout.info(`download request for ${filename} from ${uri}`);
             zout.debug(`-target filename: "${filename}"`);
 
             if (!ignore.includes('statusCode')) {
@@ -310,9 +326,10 @@ function downloadAndSaveFile(uri, filename, ignore = []) {
                 zout.warn(`Response Rejected: ${response.errors}`);
                 zout.error(`the error was: ${err}`);
                 zout.error(`error body: ${body}`)
-                zout.error(`error body: ${JSON.stringify(body)}`)
+                zout.error(`error body (unwrapped): ${JSON.stringify(body)}`)
                 reject(response);
             }
+
             else {
                 zout.info(`Response Accepted! Downloading the file...`)
                 //actually download the file
