d9db54e106eabb97bc621f70c953204562713e4e
Zoltan Tompa <zoltan@resdiary.com>
2020-06-17 10:31:04 +0100

    adding basic structure (with placeholders)

---
+++ FrontEnd/src/components/ContentHeader.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Row, Col, Icon } from 'react-materialize';
+
+
+///section css
+import componentCSS from './ContentHeader.scss'
+
+class ContentHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showSearchBar: false
+        };
+
+        this.showHide = this.showHide.bind(this);
+        this.getTitleWrapperClasses = this.getTitleWrapperClasses.bind(this);
+        this.getSearchWrapperClasses = this.getSearchWrapperClasses.bind(this);
+    }
+
+    showHide() {
+        // this is the way to Correct State overwrite
+        this.setState((state) => ({
+            showSearchBar: !state.showSearchBar
+        }));
+    }
+
+    getTitleWrapperClasses() {
+        return this.state.showSearchBar ? "title s8" : "title s10";
+    }
+
+    getSearchWrapperClasses() {
+        return this.state.showSearchBar ? "search-wrapper icons s3 open" : "search-wrapper icons s1";
+    }
+
+    render() {
+        return (
+            <div className="content-header">
+                <Row>
+                    <Col className={this.getTitleWrapperClasses()} style={{ backgroundColor: "aqua" }}>
+                        {this.props.title}
+                    </Col>
+                    <Col s={1} className={"icons"} style={{ backgroundColor: "limegreen" }}>
+                        <span onClick={() => {this.props.toggleFilterBar()}}>
+                            <Icon>filter_list</Icon>
+                        </span>
+                    </Col>
+                    <Col className={this.getSearchWrapperClasses()} style={{ backgroundColor: "red" }}>
+                        <span onClick={() => { this.showHide() }}>
+                            <Icon>search</Icon>
+                        </span>
+
+                        <input id="search-field" type="text" placeholder="search a title or author"/>
+
+                    </Col>
+
+                </Row>
+
+            </div>
+        );
+    }
+}
+
+ContentHeader.defaultProps = {
+    id: "ContentHeader"
+};
+
+ContentHeader.propTypes = {
+    id: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    toggleFilterBar: PropTypes.func.isRequired
+
+};
+
+export default ContentHeader;
