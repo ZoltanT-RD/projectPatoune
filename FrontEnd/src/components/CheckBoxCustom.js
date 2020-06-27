cfae55aa162a3b092aecc7cf6be829df196ba640
Sian <hosannahmccallum@hotmail.com>
2020-06-27 16:35:06 +0100

    Fixing minorcheckbox tings

--- FrontEnd/src/components/CheckBoxCustom.js
+++ FrontEnd/src/components/CheckBoxCustom.js
@@ -10,7 +10,7 @@ class CheckBoxCustom extends React.Component {
   render() {
     return (
       
-      <div class="check-box">
+      <div className="check-box">
         <Checkbox
         filledIn
         id={this.props.id}
@@ -24,13 +24,22 @@ class CheckBoxCustom extends React.Component {
     );
   }
 
+  updateState () {
+    this.setState({
+        elements: Object.assign({}, this.state.elements[1], {
+            disabled: 'true'
+        })
+    });
+  }
+
 };
 
 
+
 CheckBoxCustom.propTypes = {
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string,
-  value: PropTypes.string,
+  id: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   isDisabled: PropTypes.bool,
   onChange: PropTypes.func,
   checked: PropTypes.bool
