cab5108fd9e7945fb5db09b640514ae608db155b
Zoltan Tompa <zoltan@resdiary.com>
2020-07-13 11:33:45 +0100

    fixing mergeconflict

--- FrontEnd/src/components/PageContent.js
+++ FrontEnd/src/components/PageContent.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 import { Row, Col } from 'react-materialize';
 
-import ContentHeader from './ContentHeader';
-import Content from './Content';
+
+import LibraryPage from '../pages/sub/Library';
 
 
 ///section css
@@ -15,43 +15,20 @@ class PageContent extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            showFilterBar: false
-        };
-
-        this.showHide = this.showHide.bind(this);
-        this.getContentWrapperClasses = this.getContentWrapperClasses.bind(this);
-        this.getFilterBarClasses = this.getFilterBarClasses.bind(this);
     }
 
-    showHide(){
-        // this is the way to Correct State overwrite
-        this.setState((state) => ({
-            showFilterBar: !state.showFilterBar
-        }));
-    }
-
-    getContentWrapperClasses(){
-        return this.state.showFilterBar ? "content-wrapper s9" : "content-wrapper s12";
-    }
-
-    getFilterBarClasses(){
-        return this.state.showFilterBar ? "filter-wrapper s3" : "filter-wrapper s3 collapsed";
-    }
 
     render() {
         return (
             <div className="page-content">
-
                 <Row>
-{
-    ///fixme title is hardcoded here... think about trying Redux for global value sharing!
-}
+                    {
+                        ///fixme title is hardcoded here... think about trying Redux for global value sharing!
+                    }
                     <Col s={12}> <ContentHeader title={"Library (hardcoded)"} toggleFilterBar={this.showHide} /></Col>
                 </Row>
                 <Row className={"slide-wrapper"}>
-                    <Col className={this.getContentWrapperClasses()}><Content/></Col>
+                    <Col className={this.getContentWrapperClasses()}><Content /></Col>
                     <Col className={this.getFilterBarClasses()}><BookFilter /></Col>
                 </Row>
             </div>
